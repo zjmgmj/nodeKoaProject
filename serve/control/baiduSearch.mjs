e99cf7446de998b9f0ef1d5547048e578dd13ec5
zhjm <943257156@qq.com>
2020-12-02 15:55:07 +0800

    设置cookie

--- serve/control/baiduSearch.mjs
+++ serve/control/baiduSearch.mjs
@@ -6,46 +6,66 @@ class BaiduSearch {
   #page;
   #launchConfig;
   #url;
+  #cookie;
+  #cookieStr;
   constructor({launchConfig, url}) {
     this.#url = url || 'https://baidu.com'
     this.#launchConfig = launchConfig || {
       devtools: true, // 开启开发者控制台
-      headless: false // 开启浏览器界面
+      headless: false, // 开启浏览器界面
+      defaultViewport: {
+        width: 1920,
+        height: 900
+      }
     }
+    this.#cookieStr = 'MONITOR_WEB_ID=6bc745dc-326e-4e9b-88cb-2bb7a6d7a0d2; passport_csrf_token=d384633af8f286679dab8385390d752a; n_mh=6vELKnfA5uzIYntn5feMIiOtxZfci30gvZxOq-3V0sw; sid_guard=7aa16b91472d4e7a9dc3acfffe9e2a33%7C1605862975%7C5184000%7CTue%2C+19-Jan-2021+09%3A02%3A55+GMT; uid_tt=456ff4b3962bd35ee0f88c520fbd02a5; uid_tt_ss=456ff4b3962bd35ee0f88c520fbd02a5; sid_tt=7aa16b91472d4e7a9dc3acfffe9e2a33; sessionid=7aa16b91472d4e7a9dc3acfffe9e2a33; sessionid_ss=7aa16b91472d4e7a9dc3acfffe9e2a33; _ga=GA1.2.558510534.1606465177'
   }
   async init() {
     this.#browser = await puppeteer.launch(this.#launchConfig) 
     this.#page = await this.#browser.newPage()
   }
+  async setCookie(cookiesStr) {
+    const page = this.#page
+    const expires = 1611046975918
+    const domain = '.juejin.cn'
+    const path = '/'
+    let cookies = cookiesStr.split(';').map(pair=>{
+      let name = pair.trim().slice(0,pair.trim().indexOf('='))
+      let value = pair.trim().slice(pair.trim().indexOf('=')+1)
+      return {name, value, domain, path, expires}
+    });
+    await Promise.all(cookies.map(pair=>{
+        return page.setCookie(pair)
+    }))
+  }
   async start (params) {
     await this.init()
-    console.log(this.#url)
     try{
+      await this.setCookie(this.#cookieStr)
       await this.#page.goto(this.#url)
+      await this.#page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36')
       await this.#page.setRequestInterception(true)
+      await this.#page.on('request', request => {
+        request.continue([{headers: {cookie: this.#cookieStr}}])
+      })
       
-      // await this.#page.type('.search-input', params.searchName)
-      // const searchBtn = await this.#page.$('.search-icon')
-      // searchBtn.click()
-      const res = await this.#page.$$eval('.item', res => {
+      const resData = await this.#page.$$eval('.entry-list .item .title', async (elemets) => {
+        console.log(elemets)
+        const list = []
         return new Promise(resolve => {
-          console.log(res)
-          resolve(res)
+          for(let i=0; i<elemets.length; i++){
+            const el = elemets[i]
+            if(!el.href) continue
+            list.push({
+              title: el.outerText,
+              href: el.href
+            })
+          }
+          console.log(list)
+          resolve(list)
         })
       })
-      for(let i=0; i<res.length; i++){
-        const ele = res[i]
-        console.log(ele)
-        console.log(ele.valueOf())
-      }
-      // const elements = await this.#page.$$('.item')
-      // console.log(elements)
-      // for(let i=0; i<elements.length; i++){
-      //   const ele = elements[i]
-      //   const titleEle = await ele.$('.title')
-      //   ele.page
-      //   console.log(titleEle)
-      // }
+      console.log(resData)
       // await browser.close()
     }catch(e){
       console.log(e)
